d1c91b0087e51dae211bffe14be83bf7bde518b8
chakrikola07 <chakrikola77@gmail.com>
2025-02-13 13:03:47 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ecomm.ams3.digitaloceanspaces.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
